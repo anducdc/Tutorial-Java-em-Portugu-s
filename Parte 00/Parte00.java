c5c6a58e6bd80007856df3f80d5bfe5eda6f1af7
Anderson Alves <109828365+anducdc@users.noreply.github.com>
2024-01-29 11:48:50 -0300

    Add files via upload

---
+++ Parte 00/Parte00.java
@@ -0,0 +1,20 @@
+//Quando você iniciar a sua IDE de preferência, criar um Projeto Java e
+//depois criar uma Classe, é possível que o programa crie automaticamente a
+//seguinte estrutura básica do Java:
+
+public class Parte00 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+
+//Inicialmente não entraremos em detalhes do que cada uma desses linhas ou
+//palavras significam. Algumas pessoas memorizam e digitam essas coisas a cada
+//novo programa criando, outras preferem deixar que a IDE crie por conta própria.
+//Note que a classe foi denominada "Parte00" por mim anteriormente quando criei
+//este arquivo ao clicar em File > New > Class e que você pode escrever o que
+//achar melhor nesse caso, desde que se inicie por uma letra maiúscula.
+//A própria IDE adicionou à linha 5 o nome que eu dei ao configurar a classe.
+//Até aqui nada foi digitado no documento.
