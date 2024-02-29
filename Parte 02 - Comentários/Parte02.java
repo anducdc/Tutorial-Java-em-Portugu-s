001ab4eca6d3855ceb72b15b0da15c8dc0e6c9d7
Anderson Alves <109828365+anducdc@users.noreply.github.com>
2024-02-29 11:24:34 -0300

    Add files via upload

---
+++ Parte 02 - Comentários/Parte02.java
@@ -0,0 +1,24 @@
+//COMENTÁRIOS
+//Até aqui eu tenho escrito esses comentários dentro dos códigos iniciando as linhas
+//com barras duplas, que inclusive deixam essass linhas na cor verde dentro da IDE.
+//Todo texto entre essas barras e o fim da linha é ignorado pelo Java e não será
+//executado. Você pode experimentar apagando essas barras, o que fará com que o
+//compilador retorne um erro.
+
+public class Parte02 {
+
+	public static void main(String[] args) {
+		System.out.println("Meu nome não é Carlos");
+		//System.out.println("Meu nome é Anderson");
+
+	}
+
+}
+
+//No código acima eu criei duas linhas de impressao de texto quase iguais porém
+//adicionei as barras duplas na segunda. Se você executar esse código verá que só
+//a primeira linha aparecerá no console.
+//A utilidade disso é literalmente deixar informaçoees sobre o código para que você
+//ou outras pessoas consigar entender melhor o que está sendo feito quando retornarem
+//aqui no futuro. Também é muito útil para fazer experimentos pois você nao tem que
+//deletar seu código para remover partes dele.
---
+++ Parte 02 - Comentários/Parte02_2.java
@@ -0,0 +1,28 @@
+//Há duas formas de fazer essees comentários: as barras duplas para linhas únicas
+//ou uma barra e asterisco para linhas múltiplas, sendo necessário apenas incluir
+//no início da primeira linha ee no final da última. Tudo o que estiver no meio será
+//ignorado pelo compilador.
+
+public class Parte02_2 {
+
+	public static void main(String[] args) {
+		System.out.println("Meu nome é Anderson"); //comentário de uma linha
+		/*System.out.println("Meu nome NÃO é Santana");
+		System.out.println("Meu nome NÃO é Carlos");
+		System.out.println("Meu nome NÃO é André");
+		System.out.println("Meu nome NÃO é Tiago");
+		System.out.println("Meu nome NÃO é Emerson");
+		System.out.println("Meu nome NÃO é Davi");
+		System.out.println("Meu nome NÃO é Ernesto");*/
+
+	}
+
+}
+
+//Perceba que foram ignoradas múltiplas linhas apeenas indicando /* no início
+//e */ (o inverso) no final delas. A partir de agora todos os comentários de explicação
+//deste tutorial deevem seer feitos assim.
+
+//Lembre-se sempre de "fechar" os comentários com o */ quando acomentar assim senão
+//absolutamente tudo o que vier após /* será comentado, inclusive porções indesejadas
+//do seu código e caracteres fundamentais, como os }} que a IDE criou no final dele.
